d2419b8a7f6f3d4271d3e5c54452ed04d7ec06bb
marasinipradeep <marasinipradeep@gmail.com>
2020-09-06 11:06:14 +0930

    #If no data extra cards added on dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React,{useState,useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import Newscards from './components/NewsCards/NewsCards';
-
+import useStyles from './styles.js'
 
 const alanKey='2e5ca4e887f55da27b92d70eeafbbc3d2e956eca572e1d8b807a3e2338fdd0dc/stage'
 const App=()=>{
     const[newsArticles,setNewsArticles]=useState([]);
+    const classes=useStyles()
 
     useEffect(()=>{
         alanBtn({
@@ -21,7 +22,10 @@ const App=()=>{
     },[])
     return (
         <div>
-            <h1>Alan AI News Application</h1>
+           <div className={classes.logoContainer}>
+               <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo"/>
+
+           </div>
             <Newscards articles={newsArticles}/>
         </div>
     );
